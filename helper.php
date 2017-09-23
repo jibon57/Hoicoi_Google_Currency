265cc85c8f87ea09e06428e659dcdc761acdbf0f
Jibon Lawrence Costa <jibon57@users.noreply.github.com>
2017-09-23 17:49:21 +0800

    url update

--- helper.php
+++ helper.php
@@ -10,7 +10,7 @@ if (!function_exists('file_get_html') && !class_exists('simple_html_dom')){
 	require_once dirname(__FILE__).'/simple_html_dom.php';
 }
 if ($_GET['convert'] == "yes"){
-	$html = file_get_html('https://www.google.com/finance/converter?a='.$_GET['amount'].'&from='.$_GET['from'].'&to='.$_GET['to']);
+	$html = file_get_html('https://finance.google.com/finance/converter?a='.$_GET['amount'].'&from='.$_GET['from'].'&to='.$_GET['to']);
 	foreach($html->find('div#currency_converter_result .bld') as $e) {
 		echo iconv("ISO-8859-1", "UTF-8",$e->innertext);
 	}
